1a66d14c86dee8fe444f8d042b221cfbe12f38fa
Ryan Diercks <ryandiercks@hotmail.com>
2019-10-17 03:16:49 -0500

    first push with work

---
+++ src/components/SearchBar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  state = { term: 'lkajflkajsdf' };
+
+  onInputChange = () => {};
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
