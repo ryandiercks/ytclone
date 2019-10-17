19eb21eff2a408b1bb343ffcb4c982535ea20e97
Ryan Diercks <ryandiercks@hotmail.com>
2019-10-17 03:21:51 -0500

    update

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyAqWRAH1HCuxLS74jbOdS3DFN5UQ_KODrg';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY
+  }
+});
